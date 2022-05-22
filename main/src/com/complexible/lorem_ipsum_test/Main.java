2d77c2eb85b29d2e3dfd9ff211f4eb3ecff1b98b
ardangor <ardangorv@gmail.com>
2022-05-22 13:12:49 +0200

    stress test, changes

--- main/src/com/complexible/lorem_ipsum_test/Main.java
+++ main/src/com/complexible/lorem_ipsum_test/Main.java
@@ -17,7 +17,7 @@ public class Main {
         }
         long estimatedTime = System.currentTimeMillis() - startTime;
         //Print out measured time
-        System.out.println("Zero attribute: " + estimatedTime);
+        System.err.println("Zero attribute: " + estimatedTime);
 
         startTime = System.currentTimeMillis();
         //Measuring time below
@@ -26,7 +26,7 @@ public class Main {
         }
         estimatedTime = System.currentTimeMillis() - startTime;
         //Print out measured time
-        System.out.println("One attribute: " + estimatedTime);
+        System.err.println("One attribute: " + estimatedTime);
 
         startTime = System.currentTimeMillis();
         //Measuring time below
@@ -35,47 +35,43 @@ public class Main {
         }
         estimatedTime = System.currentTimeMillis() - startTime;
         //Print out measured time
-        System.out.println("Two attribute: " + estimatedTime);
+        System.err.println("Two attribute: " + estimatedTime);
 
         startTime = System.currentTimeMillis();
         //Measuring time below
         for(int i = 0; i < 1000000; i++) {
-            aGraph = RDFMapper.create().writeValue(new Person("Michael Grove", "Tokyo", "06303072443"));
+            aGraph = RDFMapper.create().writeValue(new Person("Michael Grove", "Tokyo", "0630303"));
         }
         estimatedTime = System.currentTimeMillis() - startTime;
         //Print out measured time
-        System.out.println("Three attribute: " + estimatedTime);
+        System.err.println("Three attribute: " + estimatedTime);
 
         startTime = System.currentTimeMillis();
         //Measuring time below
         for(int i = 0; i < 1000000; i++) {
-            aGraph = RDFMapper.create().writeValue(new Person("Michael Grove", "Tokyo", "06303072443", new Car("Ford")));
+            aGraph = RDFMapper.create().writeValue(new Person("Michael Grove", "Tokyo", "06372443", new Car("Ford")));
         }
         estimatedTime = System.currentTimeMillis() - startTime;
         //Print out measured time
-        System.out.println("Three attribute + object with on attribute: " + estimatedTime);
+        System.err.println("Three attribute + object with on attribute: " + estimatedTime);
 
         startTime = System.currentTimeMillis();
         //Measuring time below
         for(int i = 0; i < 1000000; i++) {
-            aGraph = RDFMapper.create().writeValue(new Person("Michael Grove", "Tokyo", "06303072443", new Car("Ford", "JMU-020")));
+            aGraph = RDFMapper.create().writeValue(new Person("Michael Grove", "Tokyo", "063030723", new Car("Ford", "JMU-020")));
         }
         estimatedTime = System.currentTimeMillis() - startTime;
         //Print out measured time
-        System.out.println("Three attribute + object with two attribute: " + estimatedTime);
+        System.err.println("Three attribute + object with two attribute: " + estimatedTime);
 
         startTime = System.currentTimeMillis();
         //Measuring time below
         for(int i = 0; i < 1000000; i++) {
-            aGraph = RDFMapper.create().writeValue(new Person("Michael Grove", "Tokyo", "06303072443", new Car("Ford", "JMU-020", Color.RED)));
+            aGraph = RDFMapper.create().writeValue(new Person("Michael Grove", "Tokyo", "0630307", new Car("Ford", "JMU-020", Color.RED)));
         }
         estimatedTime = System.currentTimeMillis() - startTime;
         //Print out measured time
-        System.out.println("Three attribute + object with three attribute: " + estimatedTime);
+        System.err.println("Three attribute + object with three attribute: " + estimatedTime);
 
-        /*Iterator<Statement> it = aGraph.iterator();
-        Statement element = it.next();
-
-        System.out.print(StatementResult.getSubject(element) + " " + StatementResult.getPredicate(element) + " " + StatementResult.getObject(element));*/
     }
 }
